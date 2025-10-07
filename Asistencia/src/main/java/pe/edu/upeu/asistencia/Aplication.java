a78a15bce070ac34f8647aba7a29d5652d104f1f
Your Name <rickyjersonmamani@gmail.com>
2025-10-07 09:10:24 -0500

    Proyecto Ventas

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/Aplication.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.asistencia;
-
-public class Aplication {
-    public static void main(String[] args) {
-        System.out.println("Catador de Comidas");
-        AsistenciaApplication.main(args);
-    }
-}
